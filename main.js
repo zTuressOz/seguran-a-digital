8ec1061de941251e56d0b050dfed50a6b2065ada
Matheus Fellipe Turesso <103067397+zTuressOz@users.noreply.github.com>
2024-06-05 09:27:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ numeroSenha.textContent = tamanhoSenha;
 
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const Numeros = '0123456789';
+const Simbolos = '"!@#$%¨&*()_+=-';
+const forcaSenha = document.querySelector('.forca');
 
 botoes [0].onclick = diminuiTamanho;
 botoes [1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
-    tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
     } 
 
     numeroSenha.textContent = tamanhoSenha;
@@ -19,7 +24,7 @@ function diminuiTamanho(){
 
 function aumentaTamanho(){
     if(tamanhoSenha < 30){
-    tamanhoSenha = tamanhoSenha + 1;
+    tamanhoSenha++;
     }
     
     numeroSenha.textContent = tamanhoSenha;
@@ -27,18 +32,49 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('#campo-senha')
+const checkbox = document.querySelectorAll('.checkbox')
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+for(let i = 0; i < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
 
 function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + Numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + Simbolos;
+    }
+
     let senha = '';
-    for(let i = 0; i < tamanhoSenha; i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    console.log(letrasMaiusculas [numeroAleatorio]);
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    ClassificaSenha();
+}
+
+function ClassificaSenha(){
+    if(tamanhoSenha > 20){
+    forcaSenha.classList.add('forte');;
+    } else if ( 10 < tamanhoSenha < 20) {
+       forcaSenha.classList.add('media');
+    } else if (tamanhoSenha < 10){
+        forcaSenha.classList.add('fraca');
     }
 }
 
-campoSenha.value = letrasMaiusculas;
+
 
 console.log(botoes);
